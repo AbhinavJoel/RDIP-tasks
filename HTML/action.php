29fc841bc3f35d52c30bb04acd70db03a6d831c3
AbhinavJoel <abhijoe20@gmail.com>
2020-08-27 18:54:07 +0530

    php file

---
+++ HTML/action.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your number is: <?php echo $_POST["number"]; ?><br>
+Your course is: <?php echo $_POST["course"]; ?><br>
+Your course description is: <?php echo $_POST["course_description"]; ?><br>
+Your subject interests are: <?php echo $_POST["subj"]; ?><br>
+
+</body>
+</html>
